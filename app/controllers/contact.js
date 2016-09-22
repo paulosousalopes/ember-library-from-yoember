47b9cfc66ac28949135c547ca9071e8557d42618
Paulo Henrique de Sousa <Paulo Henrique de Sousa>
2016-09-21 22:09:38 -0300

    Add actions for invitations, contacts andlibraries

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -6,11 +6,17 @@ export default Ember.Controller.extend({
   isValid: Ember.computed.and('isEmailValid','isMessageValid'),
   isDisabled: Ember.computed.not('isValid'),
   actions: {
-    sendMessage(){
-      alert(`Email: ${this.get('emailAddress')}, message: ${this.get('message')}`);
-      this.set('responseMessage', `We got your message and we’ll get in touch soon`);
-      this.set('emailAddress', '');
-      this.set('message', '');
+    sendMessage() {
+      const email = this.get('emailAddress');
+      const message = this.get('message');
+
+      const newContact = this.store.createRecord('contact', { email: email, message: message });
+      newContact.save().then((response) => {
+        alert(`Email: ${this.get('emailAddress')}, message: ${this.get('message')}`);
+        this.set('responseMessage', `We got your message and we’ll get in touch soon`);
+        this.set('emailAddress', '');
+        this.set('message', '');
+      });
     }
   }
 
